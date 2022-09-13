3d881622424442a9665c3175bd1217b1476be22b
Mayphiga Llewellyn <6310610982@engr.tu.ac.th>
2022-09-13 17:38:35 +0700

    add users

--- reg/course_subject/models.py
+++ reg/course_subject/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -14,5 +15,6 @@ class Course(models.Model):
     
     def __str__(self):
         return f"{self.id}.Code:{self.subjectCode} Name:{self.subjectName} seat:{self.seat}/{self.maxSeat}"
+    
         
     
--- reg/course_subject/views.py
+++ reg/course_subject/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from course_subject.models import Course
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def index(request):
     course = Course.objects.all()
+    user_data = User.objects.filter(is_superuser=False)
+
     return render(request, 'course_subject/index.html',{
-        'course_subject' : course
+        'course_subject' : course,
+        'user' : user_data
     })
